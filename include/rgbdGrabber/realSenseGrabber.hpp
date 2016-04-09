45d1a9ccd1b0de2dbe61447391e28d5675af961b
Julian Straub <jstraub@csail.mit.edu>
2016-04-09 15:18:52 -0400

    fixing things to play nice with cudaPcl

--- include/rgbdGrabber/realSenseGrabber.hpp
+++ include/rgbdGrabber/realSenseGrabber.hpp
@@ -3,17 +3,22 @@
  */
 
 #pragma once
-#include <librealsense/rs.hpp>
+//#include <librealsense/rs.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <rgbdGrabber/rgbdGrabberHelpers.hpp>
+
+namespace rs {
+class context;
+class device;
+}
 
 namespace rgbdGrabber {
 
 class RealSenseGrabber {
  public:
   RealSenseGrabber(uint32_t w, uint32_t h, uint32_t fps);
-  virtual ~RealSenseGrabber() {};
+  virtual ~RealSenseGrabber();
 
 //  virtual void depth_cb(const uint16_t * depth, uint32_t w, uint32_t h) {
 //  };  
@@ -22,12 +27,13 @@ class RealSenseGrabber {
 
   virtual void rgbd_cb(const uint16_t * depth, const uint8_t* rgb);
   virtual void run () ;
- private:
+ protected:
   const uint32_t w_;
   const uint32_t h_;
   const uint32_t fps_;
-  rs::context ctx_;
-  rs::device * dev_;
+ private:
+  rs::context* ctx_;
+  rs::device* dev_;
 };
 
 }
--- src/realSenseGrabber.cpp
+++ src/realSenseGrabber.cpp
@@ -1,17 +1,24 @@
 /* Copyright (c) 2016, Julian Straub <jstraub@csail.mit.edu>
  * Licensed under the MIT license. See the license file LICENSE.
  */
+#include <librealsense/rs.hpp>
 #include <rgbdGrabber/realSenseGrabber.hpp>
 
 namespace rgbdGrabber { 
 
 RealSenseGrabber::RealSenseGrabber(uint32_t w, uint32_t h, uint32_t fps)
     : w_(w), h_(h), fps_(fps) {
-  printf("There are %d connected RealSense devices.\n", ctx_.get_device_count());
-  dev_ = ctx_.get_device(0);
+  ctx_ = new rs::context();
+  printf("There are %d connected RealSense devices.\n", ctx_->get_device_count());
+  dev_ = ctx_->get_device(0);
   printf("\nUsing device 0, an %s\n", dev_->get_name());
   printf("    Serial number: %s\n", dev_->get_serial());
   printf("    Firmware version: %s\n", dev_->get_firmware_version());
+}
+
+RealSenseGrabber::~RealSenseGrabber() {
+//  delete dev_;
+  delete ctx_;
 }
 
 void RealSenseGrabber::rgbd_cb(const uint16_t * depth, const uint8_t* rgb) {
